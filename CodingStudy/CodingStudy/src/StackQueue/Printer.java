0cb241890b78dd4756e76fad97907e28d7025f1d
2016032897 <rlqja1107@hanyang.ac.kr>
2020-05-25 19:45:17 +0900

    code
    git add .
    
    dd;
    exit
    df

---
+++ CodingStudy/CodingStudy/src/StackQueue/Printer.java
@@ -0,0 +1,40 @@
+package StackQueue;
+import java.util.*;
+class Print{
+	int locate;
+	int value;
+	Print(int pri,int locate){
+		this.locate=locate;
+		this.value=pri;
+	}
+}
+public class Printer {
+	public int solution(int[] priorities,int location) {
+		
+		Queue<Print> print=new LinkedList<Print>();
+		Queue<Print> order=new LinkedList<Print>();
+		for(int i=0;i<priorities.length;i++) {
+		
+			print.add(new Print(priorities[i],i));
+		}
+	    int when_print=0;
+		while(print.size()!=0) {
+			int first_print=print.peek().value;
+			for(Print val: print) {
+				if(first_print<val.value) { 
+					break;
+				}
+				
+			}
+			print.add(print.poll());
+			order.add(print.poll());
+			
+		
+		}
+	}
+	
+	public static void main(String []args) {
+		
+	}
+
+}
