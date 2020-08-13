c04fda41d1bbed08464eccc6ce2b51027d40dfc5
Kimsuhhe <kimsu3819@gmail.com>
2020-08-13 19:28:32 +0900

    revised-dp:school_road

--- CodingStudy_sh/dp/school_road.java
+++ CodingStudy_sh/dp/school_road.java
@@ -2,42 +2,34 @@ package dp;
 
 public class school_road {
 	static int[][] puddles = { { 2, 2 } };
-	static int[][] dp;
+	static int[][] d;
 
 	public static int solution(int m, int n, int[][] puddles) {
-		dp = new int[n + 1][m + 1];
+		d = new int[n + 1][m + 1];
 
 		for (int i = 0; i < puddles.length; i++) {
-			dp[puddles[i][1]][puddles[i][0]] = -1;
+			d[puddles[i][1]][puddles[i][0]] = -1;
 		}
 
-		dp[0][1] = 1;
-		dp[1][0] = 1;
+		d[0][1] = 1;
 
 		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) { 
+			for (int j = 1; j <= m; j++) {
+				
+				if (d[i][j] == -1)
+					d[i][j] = 0;
 
-				if (dp[i][j] == -1) dp[i][j] = 0;
-
-				else {
-					if (i == 1)
-						dp[i][j] = dp[i][j - 1];
-					else if (j == 1)
-						dp[i][j] = dp[i - 1][j];
-					else
-						dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 100000007;
-				}
-
+				else
+					d[i][j] = (d[i - 1][j] + d[i][j - 1]) % 1000000007;
 			}
 		}
 
-		return dp[n][m];
+		return d[n][m];
 
 	}
 
 	public static void main(String[] args) {
 		System.out.println(solution(4, 3, puddles));
-
 	}
 
 }
